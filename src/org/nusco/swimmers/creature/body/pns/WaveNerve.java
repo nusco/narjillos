cd01a9fbf9252e5147104b1c3666938f95de599e
Paolo Perrotta <paolo.nusco.perrotta@gmail.com>
2014-06-24 23:34:53 +0200

    Double speed of WaveNerve in one direction only

--- src/org/nusco/swimmers/creature/body/pns/WaveNerve.java
+++ src/org/nusco/swimmers/creature/body/pns/WaveNerve.java
@@ -3,8 +3,13 @@ package org.nusco.swimmers.creature.body.pns;
 import org.nusco.swimmers.physics.Vector;
 
 /**
- * Given an input vector V, and its normal vector of the same length N,
- * this Nerve generates an output that goes from -N to N and back, in a sinusoidal wave.
+ * Given an input vector V, and its normal vector of the same length N, this
+ * Nerve generates an output that goes from -N to N and back, in a sinusoidal
+ * wave.
+ * 
+ * The negative part of the sinusoid has twice the amplitude of the positive
+ * part. This generates a life-like motion, where organs move slowly in one
+ * direction, and then return quickly to the original position.
  */
 public class WaveNerve implements Nerve {
 
@@ -18,7 +23,7 @@ public class WaveNerve implements Nerve {
 	public WaveNerve(double frequency) {
 		this.frequency = frequency;
 	}
-	
+
 	@Override
 	public Vector send(Vector inputSignal) {
 		double inputSignalLength = inputSignal.getLength() * AMPLITUDE;
@@ -30,6 +35,8 @@ public class WaveNerve implements Nerve {
 	private double getCurrentAmplitude() {
 		double amplitude = Math.cos(currentAngle);
 		currentAngle = update(currentAngle);
+		if (currentAngle > Math.PI)
+			currentAngle = update(currentAngle);
 		return amplitude;
 	}
 

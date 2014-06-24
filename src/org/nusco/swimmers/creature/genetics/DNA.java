9cafe30b423ab0eb45af7a6b31a723cddaf38eb9
Paolo Perrotta <paolo.nusco.perrotta@gmail.com>
2014-06-25 00:44:42 +0200

    Create new ancestor

--- src/org/nusco/swimmers/creature/genetics/DNA.java
+++ src/org/nusco/swimmers/creature/genetics/DNA.java
@@ -1,5 +1,7 @@
 package org.nusco.swimmers.creature.genetics;
 
+import java.util.Random;
+
 public class DNA {
 	public static final int MIRROR_ORGAN = 0b00000001;
 	public static final double MUTATION_RATE = 0.03;
@@ -43,33 +45,26 @@ public class DNA {
 	}
 
 	public static DNA random() {
+		long seed = new Random().nextLong();
+		System.out.println("Generate DNA based on seed: " + seed);
+		return random(seed);
+	}
+	
+	public static DNA ancestor() {
+		return DNA.random(9018779372573137080L);
+	}
+
+	private static DNA random(long seed) {
+		Random random = new Random(seed);
 		final int genomeSize = 60 * Byte.SIZE;
 		int[] genes = new int[genomeSize];
 		for (int i = 0; i < genes.length; i++) {
-			genes[i] = rnd(0, 255);
+			genes[i] = rnd(0, 255, random);
 		}
 		return new DNA(genes);
 	}
 
-	public static DNA ancestor() {
-		int NOT_MIRRORING = 0;
-		int[] genes =  new int[]{
-									NOT_MIRRORING, 60, 40, 0, 0,
-									NOT_MIRRORING, 50, 60, 30, 0,
-									NOT_MIRRORING, 50, 60, 30, 0,
-									NOT_MIRRORING, 30, 50, 30, 0,
-									NOT_MIRRORING, 30, 50, 30, 0,
-									NOT_MIRRORING, 30, 50, 30, 0,
-									NOT_MIRRORING, 30, 50, 30, 0,
-									NOT_MIRRORING, 50, 40, 20, 123,
-									NOT_MIRRORING, 50, 40, 20, 123,
-									NOT_MIRRORING, 50, 40, 20, 123,
-									NOT_MIRRORING, 50, 40, 20, 123
-								};
-		return new DNA(genes);
-	}
-
-	private static int rnd(int min, int max) {
-		return (int)(Math.random() * (max - min)) + min;
+	private static int rnd(int min, int max, Random random) {
+		return (int)(random.nextDouble() * (max - min)) + min;
 	}
 }

0c7fb2c9ede2a0ea9de4eba447da330029c09228
Paolo Perrotta <paolo.nusco.perrotta@gmail.com>
2014-07-18 21:25:28 +0200

    Add Energy class

---
+++ src/org/nusco/swimmers/creature/physics/Energy.java
@@ -0,0 +1,19 @@
+package org.nusco.swimmers.creature.physics;
+
+import org.nusco.swimmers.creature.body.MovementListener;
+import org.nusco.swimmers.creature.body.Organ;
+import org.nusco.swimmers.shared.physics.Segment;
+
+public class Energy implements MovementListener {
+
+	public Energy(long initial_energy) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void moveEvent(Segment beforeMovement, Organ organ) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

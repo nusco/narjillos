1c2620b42febe371b357d1986b1543f80c6b14b6
Paolo Perrotta <paolo.nusco.perrotta@gmail.com>
2014-10-13 00:39:00 +0200

    Add performance test
    Currently scoring 1000 cycles in a little more than 18 seconds.

---
+++ core/src/test/java/org/nusco/narjillos/PerformanceTest.java
@@ -0,0 +1,23 @@
+package org.nusco.narjillos;
+
+import org.nusco.narjillos.experiment.Experiment;
+
+public class PerformanceTest {
+
+	private final static int TICKS = 1000;
+
+	public static void main(String[] args) {
+		Experiment experiment = new Experiment("performance_test", 424242);
+
+		long startTimeMillis = System.currentTimeMillis();
+		
+		for (int i = 0; i < TICKS; i++)
+			experiment.tick();
+		
+		long endTimeMillis = System.currentTimeMillis();
+		double timeMillis = (endTimeMillis - startTimeMillis) / 1000.0;
+		double timeSeconds = Math.ceil(timeMillis * 10) / 10.0;
+		
+		System.out.println("" + TICKS + " cycles in " + timeSeconds + " seconds.");
+	}
+}

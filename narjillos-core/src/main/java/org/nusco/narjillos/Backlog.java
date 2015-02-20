7c5b891a28e5f776050859ca6601f06bb7bc92d7
Paolo "Nusco" Perrotta <paolo.nusco.perrotta@gmail.com>
2015-02-20 11:59:27 +0100

    Don't split Market Features when printing backlog

--- narjillos-core/src/main/java/org/nusco/narjillos/Backlog.java
+++ narjillos-core/src/main/java/org/nusco/narjillos/Backlog.java
@@ -4,34 +4,33 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Iterator;
 import java.util.List;
 
 /**
- * Utility program to print the first prioritized items in the backlog.
+ * Utility program to print the top of the backlog.
  */
 public class Backlog {
 	public static void main(String[] args) throws IOException {
 		final String BACKLOG_FILE = "backlog.md";
 		List<String> lines = loadEntries(BACKLOG_FILE);
-		print(lines, getNumberOfEntriesToPrint(args));
+		print(lines, getMinimumNumberOfEntriesToPrint(args));
 	}
 
 	private static List<String> loadEntries(final String BACKLOG_FILE) throws IOException {
 		return Files.readAllLines(Paths.get(BACKLOG_FILE), Charset.forName("UTF-8"));
 	}
 
-	private static int getNumberOfEntriesToPrint(String[] args) {
+	private static int getMinimumNumberOfEntriesToPrint(String[] args) {
 		return args.length > 0 ? Integer.parseInt(args[0]) : Integer.MAX_VALUE;
 	}
 
-	private static void print(List<String> lines, int numberOfEntriesToPrint) {
+	private static void print(List<String> lines, int minimumNumberOfEntriesToPrint) {
 		int items = 0;
 		for (String line : lines) {
 			System.out.println(line);
 			if (line.startsWith("*"))
 				items++;
-			if (items >= numberOfEntriesToPrint)
+			else if (items >= minimumNumberOfEntriesToPrint)
 				return;
 		}
 	}

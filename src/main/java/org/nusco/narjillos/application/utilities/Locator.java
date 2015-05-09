7e0fabfaaed8a6f5412987d9c0206a54e0fcad30
Andrea Cisternino <a.cisternino@gmail.com>
2015-05-09 22:37:08 +0200

    Added radius parameter to some methods in the Locator class.
    This change allows a parametric search for things in the Culture.

--- src/main/java/org/nusco/narjillos/application/utilities/Locator.java
+++ src/main/java/org/nusco/narjillos/application/utilities/Locator.java
@@ -5,9 +5,16 @@ import java.util.List;
 
 import org.nusco.narjillos.core.physics.Vector;
 import org.nusco.narjillos.core.things.Thing;
+import org.nusco.narjillos.creature.Egg;
+import org.nusco.narjillos.creature.Narjillo;
 import org.nusco.narjillos.ecosystem.Culture;
 
+/**
+ * Provides methods to find {@link Thing}s in a {@link Culture}.
+ */
 public class Locator {
+
+	private static final double DEFAULT_RADIUS = 100D;
 
 	private final Culture culture;
 
@@ -15,24 +22,68 @@ public class Locator {
 		this.culture = culture;
 	}
 
+	/**
+	 * Finds a {@link Thing} in a {@link Culture} using a custom minimum radius for the Thing.
+	 * <p>
+	 * The minimum radius is used to artificially <i>enlarge</i> the things being searched
+	 * allowing better control on the precision of the search process.
+	 *
+	 * @param position where to find a Thing.
+	 * @param thingMinRadius while searching, the radius of things is boosted to this value if smaller.
+	 * @return the first found Thing or null if nothing is present at the given position.
+	 */
+	public Thing findThingAt(Vector position, double thingMinRadius) {
+		Thing result = findWithLabel(position, thingMinRadius, "food");
+
+		if (result != null)
+			return result;
+
+		result = findWithLabel(position, thingMinRadius, "egg");
+
+		if (result != null)
+			return result;
+
+		return findNarjilloAt(position, thingMinRadius);
+	}
+
+	/**
+	 * Finds a {@link Thing} in a {@link Culture} using the default minimum radius of 100.
+	 *
+	 * @param position where to find a Thing.
+	 * @return the found Thing or null if nothing is present at the given position.
+	 */
 	public Thing findThingAt(Vector position) {
-		Thing result = findThingAt_WithLabel(position, "food");
-
-		if (result != null)
-			return result;
-
-		result = findThingAt_WithLabel(position, "egg");
-
-		if (result != null)
-			return result;
-
-		return findNarjilloAt(position);
+		return findThingAt(position, DEFAULT_RADIUS);
 	}
 
+	/**
+	 * Finds a {@link Narjillo} in a {@link Culture} using a custom minimum radius for the Thing.
+	 * <p>
+	 * The minimum radius is used to artificially <i>enlarge</i> the things being searched
+	 * allowing better control on the precision of the search process.
+	 *
+	 * @param position where to find a Thing.
+	 * @param thingMinRadius while searching, the radius of things is boosted to this value if smaller.
+	 * @return the found Narjillo or null if nothing is present at the given position.
+	 */
+	public Thing findNarjilloAt(Vector position, double thingMinRadius) {
+		return findWithLabel(position, thingMinRadius, "narjillo");
+	}
+
+	/**
+	 * Finds a {@link Narjillo} in a {@link Culture} using the default minimum radius of 100.
+	 *
+	 * @param position where to find a Thing.
+	 * @return the found Narjillo or null if nothing is present at the given position.
+	 */
 	public Thing findNarjilloAt(Vector position) {
-		return findThingAt_WithLabel(position, "narjillo");
+		return findWithLabel(position, DEFAULT_RADIUS, "narjillo");
 	}
 
+	/**
+	 * Returns a random {@link Narjillo} or {@link Egg} in the {@link Culture}.
+	 * @return the found thing.
+	 */
 	public Thing findRandomLivingThing() {
 		List<Thing> allThings = new LinkedList<>();
 		allThings.addAll(culture.getThings("narjillo"));
@@ -40,13 +91,14 @@ public class Locator {
 		return allThings.get((int)(Math.random() * allThings.size()));
 	}
 
-	private Thing findThingAt_WithLabel(Vector position, String label) {
+	private Thing findWithLabel(Vector position, double thingMinRadius, String label) {
 		Thing result = null;
 		double minDistance = Double.MAX_VALUE;
 
 		for (Thing thing : culture.getThings(label)) {
 			double distance = thing.getCenter().minus(position).getLength();
-			double radius = Math.max(thing.getRadius(), 100);
+			double radius = Math.max(thing.getRadius(), thingMinRadius);
+
 			if (distance < radius && distance < minDistance) {
 				minDistance = distance;
 				result = thing;

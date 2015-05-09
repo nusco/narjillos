d49d0edd1a35daf4b9a9a4b28cdb4e9470b71e15
Andrea Cisternino <a.cisternino@gmail.com>
2015-05-09 22:38:06 +0200

    Fixed tests for the Locator class.

--- src/test/java/org/nusco/narjillos/application/utilities/LocatorTest.java
+++ src/test/java/org/nusco/narjillos/application/utilities/LocatorTest.java
@@ -14,11 +14,11 @@ import org.nusco.narjillos.ecosystem.Ecosystem;
 import org.nusco.narjillos.genomics.DNA;
 
 public class LocatorTest {
-	
+
 	Ecosystem ecosystem;
 	Locator locator;
 	RanGen ranGen = new RanGen(1234);
-	
+
 	@Before
 	public void initialize() {
 		ecosystem = new Ecosystem(1000, false);
@@ -26,35 +26,51 @@ public class LocatorTest {
 	}
 
 	private Narjillo insertNarjillo(Vector position) {
+		// the position specifies the head position; the body center will be different
 		DNA dna = new DNA(1, "{145_227_116_072_163_201_077_221_217}{060_227_157_252_209_149_056_114_167}{250_253_092_189_010_247_016_214_009}{027_039_203_179_042_042_175_110_008}");
 		Narjillo narjillo = new Narjillo(dna, position, 90, Energy.INFINITE);
 		ecosystem.insertNarjillo(narjillo);
 		return narjillo;
 	}
-	
+
 	@Test
 	public void findsThingsAtAGivenPosition() {
 		Narjillo narjillo1 = insertNarjillo(Vector.cartesian(1000, 1000));
 		Narjillo narjillo2 = insertNarjillo(Vector.cartesian(100, 100));
+
+		// the narjillos have radius = 2.5
 		assertTrue(narjillo1.getRadius() > 2 && narjillo1.getRadius() < 3);
 		assertTrue(narjillo2.getRadius() > 2 && narjillo2.getRadius() < 3);
 
-		assertNull(locator.findNarjilloAt(Vector.cartesian(103, 103)));
-		assertEquals(narjillo2, locator.findNarjilloAt(Vector.cartesian(102, 102)));
-		assertEquals(narjillo1, locator.findNarjilloAt(Vector.cartesian(998, 1002)));
+		// point 4.24 units away from head, 3.04 from center
+		assertNull(locator.findNarjilloAt(Vector.cartesian(103, 103), 1));
+
+		// point 2.82 units away from head, 2.06 from center (-> within the radius)
+		assertEquals(narjillo2, locator.findNarjilloAt(Vector.cartesian(102, 102), 1));
+		assertEquals(narjillo1, locator.findNarjilloAt(Vector.cartesian(998, 1002), 1));
 	}
-	
+
+	@Test
+	public void findsExpandedNarjillo() {
+		Narjillo narjillo1 = insertNarjillo(Vector.cartesian(100, 100));
+
+		// original narjillo -> radius = 2.5
+		assertNull(locator.findNarjilloAt(Vector.cartesian(110, 110), 1));
+		// expanded narjillo -> radius = 20
+		assertEquals(narjillo1, locator.findNarjilloAt(Vector.cartesian(110, 110), 20));
+	}
+
 	@Test
 	public void returnsNullIfNoThingIsCloseEnough() {
 		insertNarjillo(Vector.cartesian(100, 10));
-		
+
 		assertNull(locator.findNarjilloAt(Vector.cartesian(500, 500)));
 	}
 
 	@Test
 	public void returnsNullIfTheEcosystemContainsNoNarjillos() {
 		Locator emptyLocator = new Locator(new Ecosystem(1000, false));
-		
+
 		assertNull(emptyLocator.findNarjilloAt(Vector.cartesian(150, 150)));
 	}
 }

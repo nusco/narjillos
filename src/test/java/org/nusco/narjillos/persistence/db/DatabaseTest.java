042e5f5c60f6247c1adf01f76b255c25fd99ded3
Paolo Perrotta <paolo.nusco.perrotta@gmail.com>
2015-08-07 23:38:04 +0200

    New test tasks in gradle
    Separated tasks for standard tests and database tests.

---
+++ src/test/java/org/nusco/narjillos/persistence/db/DatabaseTest.java
@@ -0,0 +1,21 @@
+package org.nusco.narjillos.persistence.db;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.nusco.narjillos.core.chemistry.Atmosphere;
+import org.nusco.narjillos.core.physics.Vector;
+import org.nusco.narjillos.core.utilities.RanGen;
+import org.nusco.narjillos.genomics.DNA;
+
+public class DatabaseTest {
+
+//	@Test
+//	public void fails() {
+//	}
+}
